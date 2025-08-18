9fd177e15b3e8440873be5df1951ddbce7ff6af8
mickaelrei <mickael.reichert@gmail.com>
2025-08-18 11:47:06 -0300

    added README.md and curve area sum test

---
+++ README.md
@@ -0,0 +1,20 @@
+# Math Expressions
+
+Create math expressions directly with C++ operators and evaluate them. Example:
+
+```cpp
+// Create variable and function
+Variable x("x");
+auto f = x*x - 10*x + 16; // x² - 10x + 16
+
+// Create context for variables
+VariableContext ctx;
+ctx["x"] = 8;
+
+// Evaluate function
+printf("f(8) = %.3f\n", f.eval(ctx)); // f(8) = 0.000
+```
+
+## Supported operations
+
+For now only the 4 basic operations are supported: addition, subtraction, multiplication and division.
--- main.cpp
+++ main.cpp
@@ -5,22 +5,41 @@
 #include "binary_operation.hpp"
 
 int main(int argc, char **argv) {
-    // Build variable and expression function
-    Variable x("x");
-    auto f = x*x - 10*x + 16;
-
-    // Attempt to get value from argument
-    float val = 2.0f;
+    // Get upper bound from args
+    float upperBound = 1.0f;
     if (argc > 1) {
-        val = atof(argv[1]);
+        upperBound = atof(argv[1]);
     }
 
-    // Create variable context
-    VariableContext ctx;
-    ctx["x"] = val;
+    // Calculate integral of x² from 0 to upperBound
+    Variable x("x");
+    const auto f = x*x;
 
-    // Testing output
-    printf("f(x) = x² - 10x + 16 | x = %.3f | f(x) = %.3f\n", val, f.eval(ctx));
+    const float dt = 0.0001f;
+    float area = 0.0f;
+    VariableContext ctx;
+    for (float t = 0.0f; t < std::abs(upperBound); t += dt) {
+        ctx["x"] = t;
+        area += f.eval(ctx) * dt;
+    }
+    if (upperBound < 0.0f) {
+        area = -area; // Flip integral bounds
+    }
+
+    // Calculate actual area
+    const auto integral = x*x*x / 3;
+    float realArea = 0.0f;
+
+    // Eval as f(upperBound) - f(0)
+    ctx["x"] = upperBound;
+    realArea += integral.eval(ctx); // eval at x=upperBound
+    ctx["x"] = 0.0f;
+    realArea -= integral.eval(ctx); // eval at x=0
+    
+    // Compare result with actual area
+    printf("real area: %.5f\n", realArea);
+    printf("calculated area: %.5f\n", area);
+    printf("difference: %.5f\n", std::abs(realArea - area));
 
     return 0;
 }
